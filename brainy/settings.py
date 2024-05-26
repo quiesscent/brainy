633b5b80b507b5e0ed1f6462d66c5cd5f08cfa4d
quiesscent <ciphedc@gmail.com>
2024-05-26 18:11:47 +0300

    code

--- brainy/settings.py
+++ brainy/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-enetdicjhh85a_chzjfm9_l!raxulef194%6@!vbh5#2dpsvc)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'brainy.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
