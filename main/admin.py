f4a82390295b4484a53ebb27bdb973768acd6c7f
Ephesians Lewis <ciphedc@gmail.com>
2024-05-31 21:22:34 +0300

    Updating Admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import CustomUser, Subject, Lesson, Score, UserProfile, Test, Answer
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ('wide',),
+                "fields": ("username", "email", "first_name", "last_name","grade", "password1", "password2"),
+            },
+        ),
+    )
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Subject)
+admin.site.register(Lesson)
+admin.site.register(Score)
+admin.site.register(Test)
+admin.site.register(Answer)
+admin.site.register(UserProfile)
